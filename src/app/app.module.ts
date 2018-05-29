6ccd1446ffaf2033b0886e5268d9fc83aaabd805
jrdisser <jimdisser@altocirrusapps.com>
2018-05-28 20:58:55 -0600

    consolodated components to match question model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { QuestionText } from './questions/question/question-text/question-text.component';
-import { QuestionDescriptionComponent } from './questions/question/question-description/question-description.component';
 import { QuestionComponent } from './questions/question/question.component';
-import { QuestionDifficultyComponent } from './questions/question/question-difficulty/question-difficulty.component';
 import { QuestionTopicsComponent } from './questions/question/question-topics/question-topics.component';
 import { QuestionReferencesComponent } from './questions/question/question-references/question-references.component';
 import { QuestionChoicesComponent } from './questions/question/question-choices/question-choices.component';
@@ -18,10 +14,7 @@ import { QuestionsComponent } from '../app/questions/questions.component';
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionText,
-    QuestionDescriptionComponent,
     QuestionComponent,
-    QuestionDifficultyComponent,
     QuestionTopicsComponent,
     QuestionReferencesComponent,
     QuestionChoicesComponent,
